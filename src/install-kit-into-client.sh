a9353e568711a03055821411ede6915e4bd6e8e0
Stoney Jackson <dr.stoney@gmail.com>
2022-07-07 15:44:28 +0000

    feat: install kit into client after clone

---
+++ src/install-kit-into-client.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+SCRIPT_PATH="${BASH_SOURCE[0]}"
+SCRIPT_NAME="$( basename -- "${SCRIPT_PATH}" )"
+SCRIPT_DIR="$( cd -- "$( dirname -- "${SCRIPT_PATH}" )" &> /dev/null && pwd )"
+SCRIPT_DIR_NAME="$( basename -- ${SCRIPT_DIR} )"
+
+main() {
+    exit-if-not-installed-as-a-git-hook
+    uninstall-this-git-hook
+    install-kit-into-client
+}
+
+exit-if-not-installed-as-a-git-hook() {
+    PARENT="$(basename -- "$SCRIPT_DIR" )"
+    GRAND_PARENT="$(basename -- "$(dirname -- "$SCRIPT_DIR" )" )"
+    if [[ "$GRAND_PARENT/$PARENT" != ".git/hooks" ]] ; then
+        echo "Not running as a git hook."
+        exit 0
+    fi
+}
+
+uninstall-this-git-hook() {
+    mv "${SCRIPT_PATH}" "${SCRIPT_DIR}/${SCRIPT_NAME}.disabled"
+}
+
+install-kit-into-client() {
+    (
+        cd "$SCRIPT_DIR/../.."
+        ./.kit/install-into-client.sh
+    )
+}
+
+main "$@"
